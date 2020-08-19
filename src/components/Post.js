db01b7e75fdb3173c51f72049e9293c97b18ee42
pranav589 <pranav.bakale@gmail.com>
2020-08-19 15:53:05 +0530

    1 file modified

--- src/components/Post.js
+++ src/components/Post.js
@@ -2,19 +2,19 @@ import React from 'react'
 import  '../Post.css'
 import Avatar from '@material-ui/core/Avatar'
 
-function Post(){
+function Post({username,caption,imageUrl}){
   return(
     <div className="post">
     <div className='postHeader'>
       <Avatar className='postAvatar' alt="Pranav" src="">P</Avatar>
-      <h3>Username</h3>
+      <h3>{username}</h3>
     </div>
        {/*header = profile pic + username */}
 
        {/*img */}
       <img  className='postImg' src="https://stackblitz.com/files/instagram-clone-using-react-and-firebase/github/pranav589/instagram-clone-using-react-and-firebase/master/Instagram-name-logo-transparent-PNG.png"/>
 
-      <h4 className='postText'><strong>Username:</strong> captions</h4>
+      <h4 className='postText'><strong>{username}:</strong> {caption}</h4>
        {/*username+captions */}
 
     </div>
