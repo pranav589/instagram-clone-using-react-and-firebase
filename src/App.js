397c10807e173f35d0514287c23acf1ed9569e48
pranav589 <pranav.bakale@gmail.com>
2020-08-19 13:12:44 +0530

    3 files modified, 2 added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from "react";
 import "./style.css";
+import Post from './components/Post'
 
 export default function App() {
-  return (
-    <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+  return(
+    <div className='app'>
+      <div className='appHeader'>
+        <img className='appHeaderImg'
+        src="https://stackblitz.com/files/instagram-clone-using-react-and-firebase/github/pranav589/instagram-clone-using-react-and-firebase/master/Instagram-name-logo-transparent-PNG.png" height="70px" width="100px"/>
+      </div>
+       <Post username="Pranav Bakale" imageUrl="https://stackblitz.com/files/instagram-clone-using-react-and-firebase/github/pranav589/instagram-clone-using-react-and-firebase/master/Instagram-name-logo-transparent-PNG.png" caption="Hello React"/>
+       <Post/>
+       <Post/>
+       <Post/>
     </div>
   );
 }
