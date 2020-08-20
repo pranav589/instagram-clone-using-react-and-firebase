5be90de14a02039b0bf9147e0b7aa90933460f23
pranav589 <pranav.bakale@gmail.com>
2020-08-20 16:29:25 +0530

    3 files modified

--- src/components/ImageUpload.js
+++ src/components/ImageUpload.js
@@ -1,6 +1,7 @@
 import React,{useState} from 'react'
 import {Button} from '@material-ui/core'
 import {storage,db} from '../firebase'
+import firrbase from 'firebase'
 
 function ImageUpload({username}){
   const [caption,setCaption]=useState('')
@@ -12,46 +13,38 @@ function ImageUpload({username}){
     if(event.target.files[0]){
       setImage(event.target.files[0])
     }
+    
   }
 
   const handleUpload=()=>{ 
-    {/*pushing the img to firebase storage*/}
-     const uploadTask=storage.ref(`images/${image.name}`).put(image)
+    const uploadTask=storage.ref(`images/${image.name}`).put(image)
 
-     uploadTask.on('state_changed',(snapshot)=>{
-       //the progress bar functiom
-       const progress=Math.round((snapshot.bytesTranferred/snapshot.totalBytes)*100)
-
-       setProgress(progress)
-     },
-     (error)=>{
-       {/*errr function */}
-       console.log(error)
-       alert(error.message)
-     },
-     ()=>{
-       {/*complete function comes here */}
-       {/*getting the download url from firebase storage */}
-       storage.ref('images').child(image.name).getDownloadURL().then(url=>{
-         db.collection('posts').add({
-           timestamp:firebase.firestore.FieldValue.serverTimestamp(),
-           caption:caption,
-           imageUrl:url,
-           username:username
-         })
-
-         setProgress(0)
-         setCaption('')
-         setImage(null)
+    uploadTask.on('state_changed',(snapshot)=>{
+      const progress=Math.round((snapshot.bytesTransferred/snapshot.totalBytes)*100)
+      setProgress(progress)
+    },(err)=>{
+      console.log(err)
+      alert(err.message)
+    },()=>{
+     storage.ref("images").child(image.name).getDownloadURL().then(url=>{console.log(url)
+       db.collection('posts').add({
+         timestamp:firebase.firestore.FieldValue.serverTimestamp(),
+         caption:caption,
+         imageUrl:url,
+         username:username
        })
-     }
-     
-     )
+       
+       
+       setProgress(0)
+       setCaption('')
+       setImage(null)
+     })
+    })
   }
 
   return(
      <div>
-     <progress value={progress} max="100"/>
+    <progress value={progress} max="100"/>
         {/*  caption input */}
         <input type='text' placeholder="write your captions here..." value={caption} onChange={event=>setCaption(event.target.value)}/>
 
