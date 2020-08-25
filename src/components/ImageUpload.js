16bf9f679001bdc521f6689fe074c3aae260da37
pranav589 <pranav.bakale@gmail.com>
2020-08-25 16:06:24 +0530

    1 file modified

--- src/components/ImageUpload.js
+++ src/components/ImageUpload.js
@@ -1,62 +1,63 @@
-import React,{useState} from 'react'
-import {Button} from '@material-ui/core'
-import {storage,db} from '../firebase'
-import db from '../firebase'
-import firrbase from 'firebase'
+import React, {useState} from 'react'
+import { Button } from '@material-ui/core'
+import { storage, db } from '../firebase'
+import firebase from 'firebase'
 
-function ImageUpload({username}){
-  const [caption,setCaption]=useState('')
-  const [image,setImage]=useState(null)
-  const [progress,setProgress]=useState(0)
+function ImageUpload({username}) {
+    const [caption, setCaption] = useState('')
+    const [image, setImage] = useState(null)
+    const [progress, setProgress] = useState(0)
 
- {/*get the first file which is selected */}
-  const handleChange=(event)=>{
-    if(event.target.files[0]){
-      setImage(event.target.files[0])
+    const handleChange = (e) => {
+        if(e.target.files[0]) setImage(e.target.files[0])
     }
-    
-  }
 
-  const handleUpload=()=>{ 
-    const uploadTask=storage.ref(`images/${image.name}`).put(image)
+    const handleUpload = () => {
+        const uploadTask = storage.ref(`images/${image.name}`).put(image)
+        uploadTask.on(
+            "state_changed",
+            (snapshot) => {
+                const progress = Math.round( 
+                    (snapshot.bytesTransferred /snapshot.totalBytes) * 100 
+                )
 
-    uploadTask.on('state_changed',(snapshot)=>{
-      const progress=Math.round((snapshot.bytesTransferred/snapshot.totalBytes)*100)
-      setProgress(progress)
-    },(err)=>{
-      console.log(err)
-      alert(err.message)
-    },()=>{
-     storage.ref("images").child(image.name).getDownloadURL().then(url=>{console.log(url)
-       db.collection("posts").add({
-         caption:caption,
-         username:username,
-         imageUrl:url,
-         timestamp:firebase.firestore.FieldValue.serverTimaddamp()
-         
-       })
-       
-       
-       setProgress(0)
-       setCaption('')
-       setImage(null)
-     })
-    })
-  }
+                setProgress(progress)
+            },
+            (error) => {
+                console.log(error)
+                alert(error.message)
+            },
+            () => {
+                storage
+                    .ref('images')
+                    .child(image.name)
+                    .getDownloadURL()
+                    .then(url => {
+                        db.collection('posts').add({
+                            timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+                            caption: caption,
+                            imageUrl: url,
+                            username: username
+                        })
+                    });
 
-  return(
-     <div>
-    <progress value={progress} max="100"/>
-        {/*  caption input */}
-        <input type='text' placeholder="write your captions here..." value={caption} onChange={event=>setCaption(event.target.value)}/>
+                    setProgress(0)
+                    setCaption('')
+                    setImage(null)
+            }
+        )
+    }
 
-        {/* file upload*/}
-         <input type='file' onChange={handleChange}/>
-        {/* button */}
-        <Button onClick={handleUpload}>Upload</Button>
-
-     </div>
-  )
+    return (
+        <div className="imageupload">
+            <progress className="imageupload__progress" value={progress} max="100" />
+            <input type="text" value={caption} placeholder="Enter a caption..." onChange={e => setCaption(e.target.value)} />
+            <input type="file" onChange={handleChange} />
+            <Button onClick={handleUpload}>
+                Upload
+            </Button>
+        </div>
+    )
 }
 
 export default ImageUpload
